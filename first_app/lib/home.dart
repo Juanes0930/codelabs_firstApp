13bcff4331cd719c35c673eb0c074d94c6ff0167
Juan Esteban <estebancode09@gmail.com>
2023-03-22 21:21:07 -0500

    Se crea el nuevo archivo (Home) y se para logica del main

---
+++ first_app/lib/home.dart
@@ -0,0 +1,90 @@
+
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return ChangeNotifierProvider(
+      create:(context) => HomeState(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'FirtsApp',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent)
+        ),
+        home: MyHomepage(),
+      ),
+   
+    );
+  }
+}
+
+// Clase MyAppState
+class HomeState extends ChangeNotifier{
+  var current = WordPair.random();
+
+  void getChangeWord() {
+    current = WordPair.random();
+      notifyListeners();   
+  }
+  
+  void getTranstaleWord() {
+    print(current);
+  }
+  
+ 
+} 
+
+class MyHomepage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    
+    var appState = context.watch<HomeState>();
+
+    return Scaffold(
+      appBar: AppBar(centerTitle: true,
+      backgroundColor: Color.fromARGB(255, 54, 105, 214),
+      title: (Title(color:  Colors.white
+      , child: Text('Generate Word' , style: TextStyle(fontSize:32 , fontWeight: FontWeight.w900 , color: Colors.white54),))),),
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: 100),
+        child: Center(
+          child: Column(children: [
+            Text('A Random Word', style: TextStyle(fontSize: 30
+            ),),
+            SizedBox(height: 5),
+            Text(appState.current.asSnakeCase, style: TextStyle(fontSize: 65, 
+            foreground: Paint()
+            ..style = PaintingStyle.stroke
+            ..strokeWidth = 4
+            ..color= Color.fromARGB(255, 54, 105, 214)),),
+            SizedBox(height: 30),
+             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:[
+                 ElevatedButton(onPressed: () => {
+                appState.getChangeWord(),
+              },  child: Text('Change')
+                    ),
+                      SizedBox(width: 30),
+                     ElevatedButton(onPressed: () => {
+                appState.getTranstaleWord(),
+              },  child: Text('Traslate')
+                    ),
+              ]
+                       )]),
+        ),
+      ),
+    );
+  }
+}
+
--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,7 +1,10 @@
 
 import 'package:english_words/english_words.dart';
+import 'package:first_app/home.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+
 
 void main() => runApp(const MyApp());
 
@@ -12,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return ChangeNotifierProvider(
-      create:(context) => MyAppState(),
+      create:(context) => HomeState(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'FirtsApp',
@@ -28,63 +31,5 @@ class MyApp extends StatelessWidget {
 }
 
 // Clase MyAppState
-class MyAppState extends ChangeNotifier{
-  var current = WordPair.random();
-
-  void getChangeWord() {
-    current = WordPair.random();
-      notifyListeners();   
-  }
-  
-  void getTranstaleWord() {
-    print(current);
-  }
-  
  
-} 
-
-class MyHomepage extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    
-    var appState = context.watch<MyAppState>();
-
-    return Scaffold(
-      appBar: AppBar(centerTitle: true,
-      backgroundColor: Color.fromARGB(255, 54, 105, 214),
-      title: (Title(color:  Colors.white
-      , child: Text('Generate Word' , style: TextStyle(fontSize:32 , fontWeight: FontWeight.w900 , color: Colors.white54),))),),
-      body: Padding(
-        padding: EdgeInsets.symmetric(vertical: 100),
-        child: Center(
-          child: Column(children: [
-            Text('A Random Word', style: TextStyle(fontSize: 30
-            ),),
-            SizedBox(height: 5),
-            Text(appState.current.asSnakeCase, style: TextStyle(fontSize: 65, 
-            foreground: Paint()
-            ..style = PaintingStyle.stroke
-            ..strokeWidth = 4
-            ..color= Color.fromARGB(255, 54, 105, 214)),),
-            SizedBox(height: 30),
-             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children:[
-                 ElevatedButton(onPressed: () => {
-                appState.getChangeWord(),
-              },  child: Text('Change')
-                    ),
-                      SizedBox(width: 30),
-                     ElevatedButton(onPressed: () => {
-                appState.getTranstaleWord(),
-              },  child: Text('Traslate')
-                    ),
-              ]
-                       )]),
-        ),
-      ),
-    );
-  }
-}
 
